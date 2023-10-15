70ac5ee4e8f60cff578d796d61e666f413230844
JumpyJacko <bobjacklee27@gmail.com>
2023-10-15 14:59:25 +1100

    Made it work with j4-menu-desktop for a sane list of applications

--- config.def.h
+++ config.def.h
@@ -79,39 +79,37 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"dmenu_run", "-m",  dmenumon,      "-fn",
-                                 dmenufont,   "-nb", col_gray1,     "-nf",
-                                 col_gray3,   "-sb", col_lightgray, "-sf",
-                                 col_gray4,   NULL};
+// static const char *dmenucmd[] = {"dmenu_run ", "-m",  dmenumon,      "-fn",
+//                                  dmenufont,    "-nb", col_gray1,     "-nf",
+//                                  col_gray3,    "-sb", col_lightgray, "-sf",
+//                                  col_gray4,    NULL};
+static const char *dmenucmd[] = {"j4-dmenu-desktop", "--dmenu='dmenu_run -m",
+                                 dmenumon,           "-fn",
+                                 dmenufont,          "-nb",
+                                 col_gray1,          "-nf",
+                                 col_gray3,          "-sb",
+                                 col_lightgray,      "-sf",
+                                 col_gray4,          "' ",
+                                 "--term='kitty'",   NULL};
 static const char *termcmd[] = {"kitty", NULL};
 // static const char *lightdown[]   = { "light", "-U", "1" };
 // static const char *lightup[]     = { "light", "-A", "1" };
 static const char *slockcmd[] = {"slock", NULL};
 static const char *hotkeyshelp[] = {
-    "kitty",        "--class",
-    "TermMemo",     "-T",
-    "hotkeys",      "-o",
-    "font_size=12", "/home/jackson/shell_scripts/dwm_hotkeys"};
-// static const char *screenshot[]  = { "maim",  "-b", "2", "-usD",
-// "/home/jackson/Images/`date +%d_%m_%Y_%H-%M-%S).png" };
+    "kitty",   "--class", "TermMemo",     "-T",
+    "hotkeys", "-o",      "font_size=12", "~/shell_scripts/dwm_hotkeys"};
 
 #include "movestack.c"
 static Key keys[] = {
     /* modifier                     key        function        argument */
-    {MODKEY, XK_space, spawn, {.v = dmenucmd}},
+    // {MODKEY, XK_space, spawn, {.v = dmenucmd}},
+    {MODKEY, XK_space, spawn,
+     SHCMD("j4-dmenu-desktop --dmenu='dmenu -i -m 0 -fn \"mononoki Nerd Font:size=24\" -nb \"#222222\" -nf \"#bbbbbb\" -sb \"#919191\" -sf \"#eeeeee\"' --term='kitty'")},
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
-    // {MODKEY, XK_F2, spawn,
-    //  SHCMD("light -U 1 && echo \"\" | dnote -id \"Brightness\" -pbar "
-    //        "$(light -G) 1.00")},
-    // {MODKEY, XK_F3, spawn,
-    //  SHCMD("light -A 1 && echo \"\" | dnote -id \"Brightness\" -pbar "
-    //        "$(light -G) 1.00")},
-    // {MODKEY, XK_F4, spawn,
-    //  SHCMD("pamixer -d 1 && echo \"\" | dnote -id \"Volume\" -pbar "
-    //        "$(pamixer --get-volume) 100")},
-    // {MODKEY, XK_F5, spawn,
-    //  SHCMD("pamixer -i 1 && echo \"\" | dnote -id \"Volume\" -pbar "
-    //        "$(pamixer --get-volume) 100")},
+    // {MODKEY, XK_F2, spawn, SHCMD("light -U 1")},
+    // {MODKEY, XK_F3, spawn, SHCMD("light -A 1")},
+    {MODKEY, XK_F4, spawn, SHCMD("pamixer -d 1")},
+    {MODKEY, XK_F5, spawn, SHCMD("pamixer -i 1")},
     {MODKEY | ControlMask, XK_h, spawn, {.v = hotkeyshelp}},
     {MODKEY | ShiftMask, XK_s, spawn,
      SHCMD(
